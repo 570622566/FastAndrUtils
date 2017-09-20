7f78886b25c491e1729a5da528bef7f327f39bc0
laijian <570622566@qq.com>
2017-09-20 17:29:54 +0800

    修改包名

---
+++ fastandrlibs/src/main/java/cn/hotapk/fastandrlibs/utils/FKeyBoardUtils.java
@@ -0,0 +1,72 @@
+package cn.hotapk.fastandrlibs.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import static android.content.Context.INPUT_METHOD_SERVICE;
+
+/**
+ * @author laijian
+ * @version 2017/8/29
+ * @Copyright (C)下午2:55 , Suntektech
+ * 键盘类
+ */
+public class FKeyBoardUtils {
+    /**
+     * 打开软键盘
+     * 魅族可能会有问题
+     *
+     * @param mEditText
+     * @param mContext
+     */
+    public static void openKeybord(EditText mEditText, Context mContext) {
+        InputMethodManager imm = (InputMethodManager) mContext
+                .getSystemService(INPUT_METHOD_SERVICE);
+        imm.showSoftInput(mEditText, InputMethodManager.RESULT_SHOWN);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,
+                InputMethodManager.HIDE_IMPLICIT_ONLY);
+    }
+
+    /**
+     * 关闭软键盘
+     *
+     * @param mEditText
+     * @param mContext
+     */
+    public static void closeKeybord(EditText mEditText, Context mContext) {
+        InputMethodManager imm = (InputMethodManager) mContext.getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(mEditText.getWindowToken(), 0);
+    }
+
+
+    /**
+     * des:隐藏软键盘,这种方式参数为activity
+     *
+     * @param activity
+     */
+    public static void hideInputForce(Activity activity) {
+        if (activity == null || activity.getCurrentFocus() == null)
+            return;
+        ((InputMethodManager) activity.getSystemService(INPUT_METHOD_SERVICE))
+                .hideSoftInputFromWindow(activity.getCurrentFocus()
+                        .getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+
+    /**
+     * 打开键盘
+     **/
+    public static void showInput(Context context, View view) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            view.requestFocus();
+            imm.showSoftInput(view, 0);
+        }
+    }
+
+
+
+
+}
---
+++ fastandrlibs/src/main/java/cn/hotapk/fastandrlibs/utils/FLogNetServer.java
@@ -0,0 +1,59 @@
+package cn.hotapk.fastandrlibs.utils;
+
+import java.io.File;
+import java.util.Map;
+
+/**
+ * @author laijian
+ * @version 2017/9/15
+ * @Copyright (C)下午4:00 , www.hotapk.cn
+ * log内网webservers
+ */
+public class FLogNetServer extends NanoHTTPD {
+
+    public FLogNetServer(int port) {
+        super(port);
+    }
+
+    public FLogNetServer(String hostname, int port) {
+        super(hostname, port);
+    }
+
+
+    @Override
+    public Response serve(IHTTPSession session) {
+        String file_name = session.getUri().substring(1);
+        StringBuffer br = new StringBuffer();
+        String header = FAssetsARawUtils.getAssetsToString("index.html");
+        br.append(header);
+        String filedir = FLogUtils.getInstance().getLogFileDir();
+        if (file_name.isEmpty()) {
+            File[] files = FFileUtils.getFiles(filedir);
+            if (files != null) {
+                for (int i = 0; i < files.length; i++) {
+                    br.append(
+                            "<div class=\"exp\"><a href = "
+                                    + "/log?logfile="
+                                    + files[i].getName() +
+                                    " target=\"_blank\" >" + files[i].getName() +
+                                    "</a ></div>"
+                    );
+                }
+
+            }
+        } else {
+            Map<String, String> parms = session.getParms();
+            if (parms.containsKey("logfile")) {
+                String name = parms.get("logfile");
+                String logstr = FFileUtils.readFile(filedir + "/" + name);
+                br.append(logstr);
+            } else {
+                br.append("有问题");
+            }
+
+        }
+        br.append("</body></html>");
+
+        return newFixedLengthResponse(Response.Status.OK, "text/html", br.toString());
+    }
+}
